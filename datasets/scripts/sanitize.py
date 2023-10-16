f609364f3a67e3680e19d50a71c3917aab698a9f
klima7 <github@mail.ukasz.com>
2023-10-17 00:15:32 +0200

    Add schema sanitization stuff

---
+++ datasets/scripts/sanitize.py
@@ -0,0 +1,48 @@
+from unidecode import unidecode
+from pathlib import Path
+import re
+
+from common import load_json, save_json
+
+import click
+from tqdm import tqdm
+
+
+TRANSLATIONS_DIR_PATH = Path(__file__).parent.parent / 'components' / 'schema' / 'translations'
+
+
+def sanitize_name(name):
+    name = unidecode(name).replace('-', '_')
+    name = re.sub('[^A-Za-z0-9]+', '', name)
+    return name
+
+
+def translate_columns(input_path, output_path):
+    columns = load_json(input_path)
+    for column in tqdm(columns, desc='Sanitizing columns'):
+        column['column_name_original_pl'] = sanitize_name(column['column_name_original_pl'])
+    save_json(output_path, columns)
+    
+    
+def translate_tables(input_path, output_path):
+    tables = load_json(input_path)
+    for table in tqdm(tables, desc='Sanitizing tables'):
+        table['name_original_pl'] = sanitize_name(table['name_original_pl'])
+    save_json(output_path, tables)
+
+
+@click.command()
+@click.argument('input_name', type=str)
+@click.argument('output_name', type=str)
+def sanitize(input_name, output_name):
+    """Sanitize given schema translation by removing special characters and create new one"""
+    
+    input_dir = TRANSLATIONS_DIR_PATH / input_name
+    output_dir = TRANSLATIONS_DIR_PATH / output_name
+    
+    translate_tables(input_dir / 'table_trans.json', output_dir / 'table_trans.json')
+    translate_columns(input_dir / 'column_trans.json', output_dir / 'column_trans.json')
+
+
+if __name__ == '__main__':
+    sanitize()
