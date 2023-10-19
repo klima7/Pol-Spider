f46fb8e44ef095105fb8223208f83bf07710c8bc
klima7 <github@mail.ukasz.com>
2023-10-19 20:00:42 +0200

    RatSQL | Fix error on preprocessing finish

--- experiments/rat-sql/rat-sql/ratsql/resources/corenlp.py
+++ experiments/rat-sql/rat-sql/ratsql/resources/corenlp.py
@@ -21,7 +21,7 @@ class CoreNLP:
         self.client = corenlp.CoreNLPClient()
 
     def __del__(self):
-        self.client.stop()
+        pass
 
     def annotate(self, text, annotators=None, output_format=None, properties=None):
         try:
