dfee1d9ab1a173bdaf1e66b816a9d917586ace3b
klima7 <ukasz.klimkiewicz@gmail.com>
2023-11-28 01:23:55 +0100

    Dataset | Fix analyse_translation command

--- datasets/scripts/analyse_translation.py
+++ datasets/scripts/analyse_translation.py
@@ -19,10 +19,16 @@ def analyse(translation_name):
     
     
 def _analyse(trans_name):
-    table_names, column_names = _get_names(trans_name)
+    table_names, table_origs, column_names, column_origs = _get_names(trans_name)
     return {
-        'tables': _analyse_names(table_names),
-        'columns': _analyse_names(column_names)
+        'tables': {
+            'name': _analyse_names(table_names),
+            'orig': _analyse_names(table_origs)
+        },
+        'columns': {
+            'name': _analyse_names(column_names),
+            'orig': _analyse_names(column_origs)
+        },
     }
     
     
@@ -33,15 +39,18 @@ def _analyse_names(names):
 
 def _get_names(trans_name):
     trans = SchemaTranslation.load_by_name(trans_name)
-    table_names, column_names = [], []
+    table_names, table_origs = [], []
+    column_names, column_origs = [], []
 
     for db_id in trans:
         for table_name in trans[db_id]:
-            table_names.append(table_name)
+            table_names.append(trans[db_id][table_name].name)
+            table_origs.append(trans[db_id][table_name].orig)
             for column_name in trans[db_id][table_name]:
-                column_names.append(column_name)
+                column_names.append(trans[db_id][table_name][column_name].name)
+                column_origs.append(trans[db_id][table_name][column_name].orig)
                 
-    return table_names, column_names
+    return table_names, table_origs, column_names, column_origs
 
 
 def _get_min_max_avg_dev(numbers):
