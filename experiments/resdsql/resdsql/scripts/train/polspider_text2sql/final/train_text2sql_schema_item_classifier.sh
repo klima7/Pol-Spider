8f11739308bbf174c58b664485fc5566f3482fdf
klima7 <github@mail.ukasz.com>
2023-12-15 15:01:50 +0000

    Resdsql | Update final classifier train command

--- experiments/resdsql/resdsql/scripts/train/polspider_text2sql/final/train_text2sql_schema_item_classifier.sh
+++ experiments/resdsql/resdsql/scripts/train/polspider_text2sql/final/train_text2sql_schema_item_classifier.sh
@@ -2,14 +2,14 @@ set -e
 
 # train schema item classifier (CSpider version)
 python -u schema_item_classifier.py \
-    --batch_size 16 \
-    --gradient_descent_step 2 \
+    --batch_size 7 \
+    --gradient_descent_step 5 \
     --device "0" \
     --learning_rate 1e-5 \
     --gamma 2.0 \
     --alpha 0.75 \
-    --epochs 128 \
-    --patience 16 \
+    --epochs 64 \
+    --patience 1000 \
     --seed 42 \
     --save_path "./models/polspider_schema_item_classifier" \
     --tensorboard_save_path "./tensorboard_log/polspider_final_schema_item_classifier" \
