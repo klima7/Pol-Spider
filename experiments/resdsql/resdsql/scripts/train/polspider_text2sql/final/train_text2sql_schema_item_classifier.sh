c967f45d426b6ccd1216eaaf6f70e7d6ef1128ab
klima7 <github@mail.ukasz.com>
2023-12-15 00:01:40 +0000

    Resdsql | Update final classifier train command

--- experiments/resdsql/resdsql/scripts/train/polspider_text2sql/final/train_text2sql_schema_item_classifier.sh
+++ experiments/resdsql/resdsql/scripts/train/polspider_text2sql/final/train_text2sql_schema_item_classifier.sh
@@ -2,8 +2,8 @@ set -e
 
 # train schema item classifier (CSpider version)
 python -u schema_item_classifier.py \
-    --batch_size 1 \
-    --gradient_descent_step 16 \
+    --batch_size 16 \
+    --gradient_descent_step 2 \
     --device "0" \
     --learning_rate 1e-5 \
     --gamma 2.0 \
@@ -12,7 +12,7 @@ python -u schema_item_classifier.py \
     --patience 16 \
     --seed 42 \
     --save_path "./models/polspider_schema_item_classifier" \
-    --tensorboard_save_path "./tensorboard_log/polspider_schema_item_classifier" \
+    --tensorboard_save_path "./tensorboard_log/polspider_final_schema_item_classifier" \
     --train_filepath "./data/preprocessed_data/preprocessed_train_polspider.json" \
     --dev_filepath "./data/preprocessed_data/preprocessed_dev_polspider.json" \
     --model_name_or_path "xlm-roberta-large" \
