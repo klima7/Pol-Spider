4c50610410dff171f1945f67cbe1e027629dab81
klima7 <github@mail.ukasz.com>
2023-12-19 22:17:17 +0100

    App | Add two tabs

--- app/app.py
+++ app/app.py
@@ -1,7 +1,12 @@
+import tempfile
+import random
+from pathlib import Path
+
 import streamlit as st
 import streamlit_ace
 from streamlit_ace import st_ace
-import random
+
+from utils import get_sql_from_db, get_schema_image_from_sql, get_error_from_sql, get_schema_dict_from_sql, divide_schema_dict
 
 
 SQL_SCHEMA_PLACEHOLDER = """
@@ -14,63 +19,106 @@ CREATE TABLE zamowienia(
 )
 """
 
-def table():
+
+UPLOADS_DIR = Path('/tmp/uploads')
+
+CLAR_COLUMNS = 3
+
+
+def table(table_name, column_names):
     with st.container(border=True):
-        st.subheader('DzienWynajmu')
-        _ = st.text_input(label='nazwa tabeli', value='nazwa filmu', label_visibility='hidden', key=random.randint(0, 100000))
+        st.subheader('🗃️ '+table_name)
+        _ = st.text_input(
+            label='table name',
+            value=table_name,
+            label_visibility='hidden',
+            key=f'table_{table_name}'
+        )
         
         st.divider()
 
-        for i in range(random.randint(3, 7)):
-            _ = st.text_input(label='fnazwa', value='nazwa filmu', key=random.randint(0, 100000))
+        for column_name in column_names:
+            _ = st.text_input(
+                label='📊 ' + column_name,
+                value=column_name,
+                key=f'column_{table_name}_{column_name}'
+            )
+
+
+def uploader_enhanced(*args, **kwargs):
+    file = st.file_uploader(
+        *args,
+        **kwargs,
+        accept_multiple_files=False,
+    )
+    
+    if file is None:
+        return None
+    
+    file_path = UPLOADS_DIR / file.file_id / file.name
+    
+    if not file_path.exists():
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(file_path, 'wb') as f:
+            f.write(file.read())
+    
+    return file_path
 
 
 st. set_page_config(layout="wide")
 
 st.title('Polish Text-to-SQL')
 
-# st.text('Upload SQLite database file or enter its schema in area bellow')
+tab1, tab2, tab3 = st.tabs(["1️⃣ DB Selection", "2️⃣ DB Clarification ", "3️⃣ Chat"])
 
-db = st.file_uploader(
-    label='Upload SQLite database here or enter its schema in area bellow',
-    type=['sqlite'],
-    accept_multiple_files=False,
-)
-
-sql_schema = st_ace(
-    language='sql',
-    height=500,
-    placeholder=SQL_SCHEMA_PLACEHOLDER,
-    theme='dracula',
-    keybinding='vscode',
-    show_gutter=False,
-    font_size=16,
-    auto_update=False,
-)
-
-if False:
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        table()
-        table()
-        table()
-        
-    with col2:
-        table()
-        table()
-        
-    with col3:
-        table()
-        table()
-    
-    st.divider()
-    
-with st.expander('📥 Import / 📤 export config'):
-    db = st.file_uploader(
-        label='Import',
-        accept_multiple_files=False,
-        # label_visibility='hidden'
+with tab1:
+    db_path = uploader_enhanced(
+        label='Upload SQLite database here or enter only its schema in area bellow',
+        type=['sqlite']
     )
-    st.button(label='Export', use_container_width=True)
-    # st.button(label='📥 Import config', use_container_width=True)
+
+    sql_from_db = get_sql_from_db(db_path) if db_path else ''
+
+    schema_sql = st_ace(
+        value=sql_from_db,
+        language='sql',
+        height=500,
+        placeholder=SQL_SCHEMA_PLACEHOLDER,
+        theme='dracula',
+        keybinding='vscode',
+        show_gutter=False,
+        font_size=16,
+        auto_update=False,
+        readonly=(db_path is not None)
+    )
+    
+    schema_error = get_error_from_sql(schema_sql)
+    schema_ok = len(schema_sql) > 0 and schema_error is None
+    
+    if schema_error:
+        st.error(f'Schema Error: {schema_error}', icon="🚨")
+
+    if schema_ok:
+        schema_image = get_schema_image_from_sql(schema_sql)
+        st.image(schema_image)
+        
+        schema_dict = get_schema_dict_from_sql(schema_sql)
+        
+    # with st.expander('📥 Import / 📤 export config'):
+    #     db = st.file_uploader(
+    #         label='Import',
+    #         accept_multiple_files=False,
+    #     )
+    #     st.button(label='Export', use_container_width=True)
+
+with tab2:
+    if not schema_ok:
+        st.info('Complete previous tab first', icon='⏪')
+    else:
+        clar_columns = st.columns(CLAR_COLUMNS)
+        schema_dict_groups = divide_schema_dict(schema_dict, CLAR_COLUMNS)
+
+        for column, schema_dict_group in zip(clar_columns, schema_dict_groups):
+            with column:
+                for table_name, table_columns in schema_dict_group.items():
+                    table(table_name, table_columns)
---
+++ app/utils.py
@@ -0,0 +1,86 @@
+import subprocess
+import tempfile
+import sqlite3
+from pathlib import Path
+
+from eralchemy import render_er
+import cv2
+
+
+def get_sql_from_db(db_path):
+    sql = subprocess.check_output(["sqlite3", db_path, ".schema"])
+    sql = sql.decode('utf-8').replace(';', ';\n')
+    return sql
+
+
+def get_db_from_sql(sql):
+    path = Path(tempfile.mkdtemp()) / 'db.sqlite'
+    with sqlite3.connect(path) as conn:
+        conn.executescript(sql)
+    return path
+
+
+def get_schema_image_from_sql(sql):
+    db_path = get_db_from_sql(sql)
+    path = Path(tempfile.mkdtemp()) / 'schema.png'
+    render_er(f"sqlite:///{db_path}", str(path))
+    return cv2.imread(str(path))
+    
+    
+def get_columns_from_db(db_path, table_name):
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        query = f"pragma table_info({table_name});"
+        cursor.execute(query)
+        records = cursor.fetchall()
+        columns = [record[1] for record in records]
+        cursor.close()
+        return columns
+    
+    
+def get_tables_from_db(db_path):
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        query = "SELECT name FROM sqlite_schema WHERE type ='table' or type='column' AND name NOT LIKE 'sqlite_%';"
+        cursor.execute(query)
+        records = cursor.fetchall()
+        tables = [record[0] for record in records]
+        cursor.close()
+        return tables
+    
+
+def get_schema_dict_from_db(db_path):
+    tables = get_tables_from_db(db_path)
+    schema_dict = {table: get_columns_from_db(db_path, table) for table in tables}
+    return schema_dict
+
+
+def get_schema_dict_from_sql(sql):
+    db_path = get_db_from_sql(sql)
+    return get_schema_dict_from_db(db_path)
+
+
+def get_error_from_sql(sql):
+    if sql.strip() == 0:
+        return None
+    
+    with tempfile.TemporaryDirectory() as temp_dir:
+        path = Path(temp_dir) / 'db.sqlite'
+        try:
+            with sqlite3.connect(path) as conn:
+                conn.executescript(sql)
+            return None
+        except sqlite3.OperationalError as e:
+            return str(e)
+
+
+def divide_schema_dict(schema_dict, groups_count):
+    groups = [{} for i in range(groups_count)]
+    
+    items = sorted(schema_dict.items(), key=lambda i: len(i[1]), reverse=True)
+    for table, columns in items:
+        groups_lengths = [(sum([len(columns)+3 for columns in group.values()]), idx) for idx, group in enumerate(groups)]
+        smallest_group_idx = list(sorted(groups_lengths))[0][1]
+        groups[smallest_group_idx][table] = columns
+        
+    return groups
