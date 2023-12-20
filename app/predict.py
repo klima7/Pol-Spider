6665d9983eed068b0af3a1d2869aab58b3032f23
klima7 <github@mail.ukasz.com>
2023-12-20 21:02:53 +0100

    App | Remove debug lines

--- app/predict.py
+++ app/predict.py
@@ -6,8 +6,8 @@ import streamlit as st
 
 def predict_sql(question, sem_names):
     time.sleep(1)
-    st.write(question)
-    st.json(sem_names)
+    # st.write(question)
+    # st.json(sem_names)
     return random.choice([
         "SELECT * FROM student",
         "SELECT imie FROM student",
