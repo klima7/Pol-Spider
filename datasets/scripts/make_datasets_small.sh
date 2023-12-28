4c374a38886739c0de1be5d78f59a0522cf7e251
klima7 <ukasz.klimkiewicz@gmail.com>
2023-12-28 01:26:04 +0100

    Dataset | Add bash script for subsampling datasets

---
+++ datasets/scripts/make_datasets_small.sh
@@ -0,0 +1,14 @@
+python subsample.py pol_spider_pl pol_spider_pl_50 --dev 50 --train 0 --seed 42
+python subsample.py pol_spider_en pol_spider_en_50 --dev 50 --train 0 --seed 42
+
+python subsample.py pol_spidersyn_pl pol_spidersyn_pl_50 --dev 50 --train 0 --seed 42
+python subsample.py pol_spidersyn_en pol_spidersyn_en_50 --dev 50 --train 0 --seed 42
+
+python subsample.py pol_spiderdk_pl pol_spiderdk_pl_50 --dev 50 --train 0 --seed 42
+python subsample.py pol_spiderdk_en pol_spiderdk_en_50 --dev 50 --train 0 --seed 42
+
+python subsample.py pol_sparc_pl pol_sparc_pl_50 --dev 50 --train 0 --seed 42
+python subsample.py pol_sparc_en pol_sparc_en_50 --dev 50 --train 0 --seed 42
+
+python subsample.py pol_cosql_pl pol_cosql_pl_50 --dev 50 --train 0 --seed 42
+python subsample.py pol_cosql_en pol_cosql_en_50 --dev 50 --train 0 --seed 42
