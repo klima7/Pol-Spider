431059823e187003a8c02ecd097baa470ba69474
klima7 <github@mail.ukasz.com>
2023-12-28 18:24:19 +0100

    Dataset | Fix bash script for creating subsamples datasets

--- datasets/scripts/make_datasets_small.sh
+++ datasets/scripts/make_datasets_small.sh
@@ -7,8 +7,8 @@ python subsample.py pol_spidersyn_en pol_spidersyn_en_50 --dev 50 --train 0 --se
 python subsample.py pol_spiderdk_pl pol_spiderdk_pl_50 --dev 50 --train 0 --seed 42
 python subsample.py pol_spiderdk_en pol_spiderdk_en_50 --dev 50 --train 0 --seed 42
 
-python subsample.py pol_sparc_pl pol_sparc_pl_50 --dev 50 --train 0 --seed 42
-python subsample.py pol_sparc_en pol_sparc_en_50 --dev 50 --train 0 --seed 42
+python subsample.py pol_sparcwc_pl pol_sparc_pl_50 --dev 50 --train 0 --seed 42
+python subsample.py pol_sparcwc_en pol_sparc_en_50 --dev 50 --train 0 --seed 42
 
-python subsample.py pol_cosql_pl pol_cosql_pl_50 --dev 50 --train 0 --seed 42
-python subsample.py pol_cosql_en pol_cosql_en_50 --dev 50 --train 0 --seed 42
+python subsample.py pol_cosqlwc_pl pol_cosql_pl_50 --dev 50 --train 0 --seed 42
+python subsample.py pol_cosqlwc_en pol_cosql_en_50 --dev 50 --train 0 --seed 42
